6020f0c989f8ef49f9971df92f8f09d4fd146a37
CAUAD22 <cauad22@CAUui-iMac-7.local>
2018-07-25 12:38:37 +0900

    테이블뷰실습

---
+++ MyTable/Main.swift
@@ -0,0 +1,139 @@
+//
+//  Main.swift
+//  MyTable
+//
+//  Created by CAUAD22 on 2018. 7. 25..
+//  Copyright © 2018년 CAUAD22. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var foodTitle: UILabel!
+    @IBOutlet weak var foodImage: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
+class Food {
+    var name:String
+    var image:UIImage
+    
+    init(name:String, image:UIImage) {
+        self.name = name
+        self.image = image
+    }
+}
+
+
+class Main: UITableViewController {
+    
+    var foods:[Food] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        var food1 = Food(name: "짜장면", image: UIImage(named: "star")!)
+        var food2 = Food(name: "짬뽕", image: UIImage(named: "star")!)
+        
+        foods.append(food1)
+        foods.append(food2)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return foods.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        guard let myCell = cell as? MyTableViewTableViewCell else {
+            return cell
+        }
+
+        myCell.foodTitle.text = foods[indexPath.row].name
+        myCell.foodImage.image = foods[indexPath.row].image
+        
+        return myCell
+    }
+
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyTable/MyTableViewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewTableViewCell.swift
+//  MyTable
+//
+//  Created by CAUAD22 on 2018. 7. 25..
+//  Copyright © 2018년 CAUAD22. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
